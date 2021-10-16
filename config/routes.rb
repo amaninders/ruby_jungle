066a9d60a8ab3448742e724cef5b77112be90f04
amaninders <amandeolofficial@gmail.com>
2021-10-16 19:26:38 -0400

    Add about us

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+	get 'about' => 'about#index'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
