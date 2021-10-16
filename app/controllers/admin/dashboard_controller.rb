328f1050534b15648b8a2012b61b311230ec0119
amaninders <amandeolofficial@gmail.com>
2021-10-16 19:09:50 -0400

    Add admin dashboard items

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products = Product.order(id: :desc).all
+		@categories = Category.order(id: :desc).all
+		@orders = Order.order(id: :desc).all
   end
 end
