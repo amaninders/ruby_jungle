5cf1ef817da3697d9427fcaa75ea5c70a23b577a
amaninders <amandeolofficial@gmail.com>
2021-10-19 22:22:07 -0400

    implement the new method for user spec authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
+	
 	has_secure_password
+
+	validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 3,
+	too_short: "%{count} characters is the minimum required" }, confirmation: true
+	validates_confirmation_of :password
+  validates :password_confirmation, presence: true
+
+	def self.authenticate_with_credentials(email, password)
+    @user = self.where("lower(email) = ?", email.delete(' ').downcase).first.try(:authenticate, password)
+  end
+	
 end
