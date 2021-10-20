2845fd5ae06ce6c199897e1252f62f638a7cacbe
amaninders <amandeolofficial@gmail.com>
2021-10-19 23:07:44 -0400

    fixed sessions logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 3,
 	too_short: "%{count} characters is the minimum required" }, confirmation: true
 	validates_confirmation_of :password
